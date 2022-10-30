e8e37663e58ccde1bb205d1485c9003c3c794faa
Alla Yu. Vladova <38113304+avladova@users.noreply.github.com>
2022-10-30 15:26:49 +0300

    Add files via upload
    additional questions

---
+++ Seminar 6a & 8a geom probability&event algebra.R
@@ -0,0 +1,35 @@
+#Additional questions to Seminar 6a - Geometric probability
+
+#task 20
+#Ssquare = 2*R²
+R=25
+1-2*R^2/(pi*R^2)
+
+1-2/pi
+
+#task 21
+80/160
+
+#task 22
+pi*3.5^2/(8^2)
+
+#task 23
+120/360
+1/3
+
+#Additional questions to Seminar 8 - Event algebra - Complicated questions
+
+#Question 1
+pA=0.8
+pB=0.7
+pC=0.6
+pA+pB+pC-pA*pB-pA*pC-pB*pC+pA*pB*pC
+
+#Question 2
+pA=0.8
+pB=0.7
+pC=0.6
+pA*pB+pC-pA*pB*pC
+
+#7
+0.8*0.7*0.6/(0.7+0.6-0.3*0.6)
